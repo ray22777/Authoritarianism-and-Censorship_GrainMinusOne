bc59bb5c16ce4a2a83313cd188c4d3b0b8277d1a
Ray <221049@hchs.edu.my>
2025-06-14 11:14:40 +0800

    WHY DOES IT NOT WORKRKKKK

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import StarTunnelCanvas from "./Stars";
+import MainPage from "./MainPage";
+
 
 const App = () => {
+  const [showTunnel, setShowTunnel] = useState(false);
+
   return (
     <div>
-      <StarTunnelCanvas />
+      {showTunnel ? (
+        <StarTunnelCanvas />
+      ) : (
+        <MainPage onExplore={() => setShowTunnel(true)} />
+      )}
     </div>
-    
   );
 };
 
--- src/Stars.js
+++ src/Stars.js
@@ -290,7 +290,7 @@ const StarTunnelCanvas = () => {
           position: "fixed",
           bottom: 0,
           left: 0,
-          height: "8px",
+          height: "8spx",
           width: "0%",
           maxWidth: "100%",
           background: "linear-gradient(to right, rgba(0, 219, 0, 0.74), rgba(0, 116, 10, 0.74))",
